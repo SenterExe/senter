e704fb10c373037dc5a9385deedc7189512bb4f7
mukund-9652 <mukundravisankaran2001@gmail.com>
2023-01-15 02:04:02 +0530

    update commit

--- senter/settings.py
+++ senter/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    "django-insecure-1l)o_c%%sm=buwbxddj_yk+1_bsg2jaw=c0urzbg#z=d*et6pj"
-)
+SECRET_KEY = config('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -160,11 +160,11 @@ MESSAGE_TAGS={
     messages.ERROR:'danger',
 }
 
-EMAIL_HOST='smtp.gmail.com'
-EMAIL_PORT=587
-EMAIL_HOST_USER='senterexecutive@gmail.com'
-EMAIL_HOST_PASSWORD='rsxusiqsdxwqxvkp'
-EMAIL_USE_TLS=True
+EMAIL_HOST=config('EMAIL_HOST')
+EMAIL_PORT=config('EMAIL_PORT', cast=int)
+EMAIL_HOST_USER=config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD=config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS=config('EMAIL_USE_TLS', cast=bool)
 
 API_KEY='test_b09d34bc79d44f136863a6a7459'
 AUTH_TOKEN='test_2a827424d515a41c8184d260b03'
