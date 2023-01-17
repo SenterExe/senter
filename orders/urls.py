beca713f97fa3074aced5656df6197abd2931181
mukund-9652 <mukundravisankaran2001@gmail.com>
2023-01-17 20:10:38 +0530

    portal update commit 2

--- orders/urls.py
+++ orders/urls.py
@@ -5,6 +5,5 @@ urlpatterns=[
     path('place_order/',views.place_order, name='place_order'),
     path('payments/', views.payments, name='payments'),
     path('payments/<int:order_id>', views.paynow, name='paynow'),
-    path('payments_success/', views.paymentsuccess)
-
+    path('payments_order/', views.paymentorder),
 ]
